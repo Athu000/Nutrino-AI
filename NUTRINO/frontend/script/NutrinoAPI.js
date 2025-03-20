c22609357b131e9da75a06533de4d2e65d4c90e2
Athu000 <atharvajadhav0001@gmail.com>
2025-03-20 00:48:29 -0700

    Update NutrinoAPI.js

--- NUTRINO/frontend/script/NutrinoAPI.js
+++ NUTRINO/frontend/script/NutrinoAPI.js
@@ -1,4 +1,5 @@
 import { auth, db } from "./auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { collection, addDoc, getDocs, query, where, deleteDoc, doc, orderBy, limit  } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const API_BASE_URL = "https://nutrino-ai.onrender.com/api";
